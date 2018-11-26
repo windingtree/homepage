c81c382f21b05a09a9d655f2339f16c6a778e2b2
Nico Calabrese <nico.sikker@gmail.com>
2018-11-26 12:53:20 -0300

    Removed comments

--- assets/js/components/roadmap.js
+++ assets/js/components/roadmap.js
@@ -6,11 +6,9 @@ $(function () {
 
     $roadmapToggleBtn.on('click', function () {
         if (this.getAttribute('aria-expanded') === 'true') {
-            console.log('opened');
             $(this).find('.mdi').addClass('mdi-plus-circle');
             $(this).find('.mdi').removeClass('mdi-minus-circle-outline');
         } else {
-            console.log('closed');
             $(this).find('.mdi').addClass('mdi-minus-circle-outline');
             $(this).find('.mdi').removeClass('mdi-plus-circle');
         }
