9f0f8398b8352289e84416c6ff99d1e7dfeae805
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2018-12-05 12:03:07 -0300

    feat(KeyBenefits): Add to Sellers and Suppliers view

--- src/features/Sellers/index.jsx
+++ src/features/Sellers/index.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import HowItWorks from 'commons/HowItWorks';
+import KeyBenefits from 'commons/KeyBenefits';
 
 const Sellers = () => (
   <div>
+    <KeyBenefits>
+      <KeyBenefits.OpenMarketplace />
+      <KeyBenefits.SetYourCommission />
+      <KeyBenefits.NetworkWithoutBorders />
+      <KeyBenefits.ControlYourData />
+      <KeyBenefits.PersonalizedTravel />
+      <KeyBenefits.NewBusinessModel />
+    </KeyBenefits>
     <HowItWorks />
   </div>);
 
