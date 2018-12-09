98da82d8fc3f71632d65224a5bc07ccbf8ebb106
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2018-12-09 19:41:58 -0300

    feat(LifToken): Add ICO stats and key benefits sections to view

--- src/features/LifToken/index.jsx
+++ src/features/LifToken/index.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import KeyBenefits from 'commons/KeyBenefits';
+import LifIco from './LifIco';
 
-const LifToken = () => <div />;
+const LifToken = () => (
+  <>
+    <KeyBenefits noHeader>
+      <KeyBenefits.Lead>
+        Líf is the most important underlying technology of the Winding Tree platform.
+        It is used for two purposes:
+      </KeyBenefits.Lead>
+      <KeyBenefits.PaymentsOnPlatform />
+      <KeyBenefits.PlatformGovernance />
+    </KeyBenefits>
+    <LifIco />
+  </>
+);
 
 export default LifToken;
