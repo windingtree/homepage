1c3b933b6494bb2f6d1d9d4c3679667e61264d98
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2018-12-11 14:29:50 -0300

    feat(Contributors): Add Slick

---
+++ src/commons/Contributors/contributors.js
@@ -0,0 +1,239 @@
+const contributors = [
+  {
+    id: 7763867,
+    login: 'AugustoL',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/7763867?v=4',
+  },
+  {
+    id: 45091,
+    login: 'kvbik',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/45091?v=4',
+  },
+  {
+    id: 375483,
+    login: 'JirkaChadima',
+    avatar_url: 'https://avatars3.githubusercontent.com/u/375483?v=4',
+  },
+  {
+    id: 377005,
+    login: 'kvakes',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/377005?v=4',
+  },
+  {
+    id: 825045,
+    login: 'czervenka',
+    avatar_url: 'https://avatars3.githubusercontent.com/u/825045?v=4',
+  },
+  {
+    id: 1435069,
+    login: 'skkr',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/1435069?v=4',
+  },
+  {
+    id: 4557491,
+    login: 'menschikov',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/4557491?v=4',
+  },
+  {
+    id: 5481398,
+    login: 'MatiasOS',
+    avatar_url: 'https://avatars1.githubusercontent.com/u/5481398?v=4',
+  },
+  {
+    id: 11988423,
+    login: 'PabloAlice',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/11988423?v=4',
+  },
+  {
+    id: 81371,
+    login: 'nberger',
+    avatar_url: 'https://avatars1.githubusercontent.com/u/81371?v=4',
+  },
+  {
+    id: 11652019,
+    login: 'cwhinfrey',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/11652019?v=4',
+  },
+  {
+    id: 22138672,
+    login: 'adklempner',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/22138672?v=4',
+  },
+  {
+    id: 916414,
+    login: 'horyd',
+    avatar_url: 'https://avatars3.githubusercontent.com/u/916414?v=4',
+  },
+  {
+    id: 7332026,
+    login: 'cgewecke',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/7332026?v=4',
+  },
+  {
+    id: 23040076,
+    login: 'greenkeeper[bot]',
+    avatar_url: 'https://avatars3.githubusercontent.com/in/505?v=4',
+  },
+  {
+    id: 5726606,
+    login: 'hynek-urban',
+    avatar_url: 'https://avatars3.githubusercontent.com/u/5726606?v=4',
+  },
+  {
+    id: 1965274,
+    login: 'beogip',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/1965274?v=4',
+  },
+  {
+    id: 1389619,
+    login: 'ppoliani',
+    avatar_url: 'https://avatars1.githubusercontent.com/u/1389619?v=4',
+  },
+  {
+    id: 3124707,
+    login: 'mikec',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/3124707?v=4',
+  },
+  {
+    id: 16988745,
+    login: 'ganes1410',
+    avatar_url: 'https://avatars3.githubusercontent.com/u/16988745?v=4',
+  },
+  {
+    id: 5529152,
+    login: 'fMercury',
+    avatar_url: 'https://avatars3.githubusercontent.com/u/5529152?v=4',
+  },
+  {
+    id: 601027,
+    login: 'Jkat',
+    avatar_url: 'https://avatars1.githubusercontent.com/u/601027?v=4',
+  },
+  {
+    id: 1547890,
+    login: 'umran',
+    avatar_url: 'https://avatars1.githubusercontent.com/u/1547890?v=4',
+  },
+  {
+    id: 111600,
+    login: 'ligi',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/111600?v=4',
+  },
+  {
+    id: 44440536,
+    login: 'umerrasheed',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/44440536?v=4',
+  },
+  {
+    id: 5802509,
+    login: 'vbledar',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/5802509?v=4',
+  },
+  {
+    id: 1938013,
+    login: 'crazyrabbitLTC',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/1938013?v=4',
+  },
+  {
+    id: 3713401,
+    login: 'kmadorin',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/3713401?v=4',
+  },
+  {
+    id: 6653762,
+    login: 'vpiskunov',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/6653762?v=4',
+  },
+  {
+    id: 44440720,
+    login: 'afklblockchain',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/44440720?v=4',
+  },
+  {
+    id: 44459129,
+    login: 'ianjianliang',
+    avatar_url: 'https://avatars1.githubusercontent.com/u/44459129?v=4',
+  },
+  {
+    id: 10225272,
+    login: 'kalvarez2',
+    avatar_url: 'https://avatars1.githubusercontent.com/u/10225272?v=4',
+  },
+  {
+    id: 36217860,
+    login: 'rakahbus',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/36217860?v=4',
+  },
+  {
+    id: 6967192,
+    login: 'facuspagnuolo',
+    avatar_url: 'https://avatars1.githubusercontent.com/u/6967192?v=4',
+  },
+  {
+    id: 287189,
+    login: 'maraoz',
+    avatar_url: 'https://avatars3.githubusercontent.com/u/287189?v=4',
+  },
+  {
+    id: 481465,
+    login: 'frangio',
+    avatar_url: 'https://avatars1.githubusercontent.com/u/481465?v=4',
+  },
+  {
+    id: 232174,
+    login: 'fzeoli',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/232174?v=4',
+  },
+  {
+    id: 10086922,
+    login: 'SublimeApple',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/10086922?v=4',
+  },
+  {
+    id: 1144763,
+    login: 'alexbainbridge',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/1144763?v=4',
+  },
+  {
+    id: 6963201,
+    login: 'ondrejchmelar',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/6963201?v=4',
+  },
+  {
+    id: 36218949,
+    login: 'traversel',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/36218949?v=4',
+  },
+  {
+    id: 32934649,
+    login: 'vecoalla',
+    avatar_url: 'https://avatars1.githubusercontent.com/u/32934649?v=4',
+  },
+  {
+    id: 41238779,
+    login: 'Spencer-WindingTree',
+    avatar_url: 'https://avatars1.githubusercontent.com/u/41238779?v=4',
+  },
+  {
+    id: 16164843,
+    login: 'luisduenas',
+    avatar_url: 'https://avatars3.githubusercontent.com/u/16164843?v=4',
+  },
+  {
+    id: 5064599,
+    login: 'brainsapiens',
+    avatar_url: 'https://avatars3.githubusercontent.com/u/5064599?v=4',
+  },
+  {
+    id: 148100,
+    login: 'invalid-email-address',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/148100?v=4',
+  },
+];
+
+const contributosToIgnore = [
+  5726606,
+  23040076,
+];
+
+export { contributors, contributosToIgnore };
