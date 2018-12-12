ed198cfae4016e2af84c088275ac9a299586b580
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2018-12-12 09:40:10 -0300

    refactor(KeyBenefits): Allow to read from file

--- src/features/Suppliers/index.jsx
+++ src/features/Suppliers/index.jsx
@@ -1,18 +1,25 @@
 /* @flow */
 import React from 'react';
-import HowItWorks from 'commons/HowItWorks';
 import KeyBenefits from 'commons/KeyBenefits';
+import HowItWorks from 'commons/HowItWorks';
 import Solutions from './Solutions';
+import keyBenefitsDescriptions from './keyBenefits.json';
 
 const Suppliers = () => (
   <div>
     <KeyBenefits>
-      <KeyBenefits.OpenMarketplace />
-      <KeyBenefits.ImmediateSettlement />
-      <KeyBenefits.NetworkWithoutBorders />
-      <KeyBenefits.ControlYourData />
-      <KeyBenefits.NoFakeReviews />
-      <KeyBenefits.NewBusinessModel />
+      {
+        keyBenefitsDescriptions.keyBenefits.map((benefit) => {
+          console.log(benefit);
+          return (
+            <KeyBenefits.Benefit
+              title={benefit.title}
+              img={benefit.icon}
+              body={benefit.body}
+              key={benefit.title}
+            />);
+        })
+      }
     </KeyBenefits>
     <HowItWorks />
     <Solutions />
