2365c82fbe1bdffdc8a5276be8a19d5cecd4ada3
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2018-12-18 10:44:57 -0300

    feat(StartupsAndDevelopers): Add AlredyWith us section

--- src/features/StartupsAndDevelopers/index.jsx
+++ src/features/StartupsAndDevelopers/index.jsx
@@ -1,9 +1,11 @@
+/* @flow */
 import React from 'react';
 import {
   Container, Row, Col,
 } from '@windingtree/wt-ui-react';
 import Contributors from 'commons/Contributors';
 import BlockQuoteSection from './BlockQuoteSection';
+import AlreadyWithUs from './AlreadyWithUs';
 
 const StartupsAndDevelopers = () => (
   <section className="already-with-us my-3 my-md-5">
@@ -14,6 +16,11 @@ const StartupsAndDevelopers = () => (
       <Row className="justify-content-md-center">
         <Col md={8}>
           <h2 className="mb-2 mb-md-4 text-center">Already With Us</h2>
+          <AlreadyWithUs />
+        </Col>
+      </Row>
+      <Row className="justify-content-md-center">
+        <Col md={8}>
           <h3 className="mb-2 mb-md-3 text-center">Developers and Contributors</h3>
         </Col>
       </Row>
