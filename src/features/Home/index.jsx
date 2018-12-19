3711b37ec768458408dc7e452fb92b1d41113dbe
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2018-12-19 10:28:54 -0300

    chore(Home): Move DesignedFor and LifToken sections

--- src/features/Home/index.jsx
+++ src/features/Home/index.jsx
@@ -1,9 +1,15 @@
 // @flow
 import React from 'react';
+import LifToken from './LifToken';
 import InThePress from './InThePress';
+import DesignedFor from './DesignedFor';
 
 const Home = () => (
-  <InThePress />
+  <>
+    <LifToken />
+    <InThePress />
+    <DesignedFor />
+  </>
 );
 
 export default Home;
