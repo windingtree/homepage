c25464e4caa71c0497f72fa38e54e50e42947adb
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2018-12-19 15:48:50 -0300

    feat(Home): Add Partners section

--- src/features/Home/index.jsx
+++ src/features/Home/index.jsx
@@ -1,12 +1,15 @@
 // @flow
 import React from 'react';
 import LifToken from './LifToken';
+import Partners from './Partners';
 import InThePress from './InThePress';
 import DesignedFor from './DesignedFor';
+
 
 const Home = () => (
   <>
     <LifToken />
+    <Partners />
     <InThePress />
     <DesignedFor />
   </>
