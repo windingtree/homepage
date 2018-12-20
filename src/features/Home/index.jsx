f8544531d6577e7771e03c768eb68e6bad4e754b
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2018-12-20 09:55:07 -0300

    fix(Home): Add .app-section container

--- src/features/Home/index.jsx
+++ src/features/Home/index.jsx
@@ -1,5 +1,8 @@
 // @flow
 import React from 'react';
+import {
+  Container,
+} from '@windingtree/wt-ui-react';
 import LifToken from './LifToken';
 import Partners from './Partners';
 import InThePress from './InThePress';
@@ -7,12 +10,12 @@ import DesignedFor from './DesignedFor';
 
 
 const Home = () => (
-  <>
+  <Container className="app-section">
     <LifToken />
     <Partners />
     <InThePress />
     <DesignedFor />
-  </>
+  </Container>
 );
 
 export default Home;
