5452d86246fe1341c518fb1d2118830963514316
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2018-12-31 10:16:14 -0300

    fix(Events): Fix tab order and selected tab animation

--- src/commons/Events/EventTabs.jsx
+++ src/commons/Events/EventTabs.jsx
@@ -7,14 +7,14 @@ import EventTab from './EventTab';
 import events from './data';
 
 const EventTabs = () => (
-  <Tab.Container id="event-tabs" defaultActiveKey="Own">
+  <Tab.Container id="event-tabs" defaultActiveKey="Upcoming">
     <Row>
-      <Nav as="ul" className="mb-2">
-        <Nav.Item as="li">
-          <Nav.Link eventKey="Own">Own Events</Nav.Link>
-        </Nav.Item>
+      <Nav as="ul" className="mb-2 nav-tabs">
         <Nav.Item as="li">
           <Nav.Link eventKey="Upcoming">Upcoming Events</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link eventKey="Own">Own Events</Nav.Link>
         </Nav.Item>
         <Nav.Item as="li">
           <Nav.Link eventKey="Past">Past Events</Nav.Link>
@@ -23,9 +23,9 @@ const EventTabs = () => (
     </Row>
     <Row>
       <Tab.Content>
-        <EventTab events={events.own} name="Own" id="Own" />
         <EventTab events={events.upcoming} name="Upcoming" id="Upcoming" />
         <EventTab events={events.past} name="Past" id="Past" />
+        <EventTab events={events.own} name="Own" id="Own" />
       </Tab.Content>
     </Row>
   </Tab.Container>
