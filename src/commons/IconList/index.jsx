6b805afcd6cf08fbc5b7e5de78794c7538b47140
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2019-01-04 16:31:29 -0300

    feat(IconList): Add component to display social icons

---
+++ src/commons/IconList/index.jsx
@@ -0,0 +1,27 @@
+/* @flow */
+import React from 'react';
+import icons from './data';
+
+type PropsType = {
+  variant: 'white' | 'black',
+  size: number
+};
+
+const IconList = (props: PropsType) => {
+  const {
+    variant, size,
+  } = props;
+  return (
+    <ul className="list-inline">
+      {icons.map(icon => (
+        <li className="list-inline-item" key={icon.href}>
+          <a href={icon.href} tittle={icon.title} className={`text-${variant} text-alpha-inverse`}>
+            <i className={`mdi mdi-${size}px ${icon.className}`} />
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default IconList;
