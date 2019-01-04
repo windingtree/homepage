a34eeada2baec637aea2df4b27463cb39a72dd0f
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2019-01-04 10:31:17 -0300

    fix(LearnMore): Show all objects always

--- src/commons/LearnMoreCards/data.js
+++ src/commons/LearnMoreCards/data.js
@@ -2,7 +2,6 @@
 import suppliersImg from './img/1.svg';
 import sellersImg from './img/2.svg';
 import vendorsImg from './img/3.svg';
-import tokenImg from './img/4.svg';
 import foundationImg from './img/5.svg';
 
 import type { DataType } from './index';
@@ -34,16 +33,6 @@ const cardsInfo: DataType[] = [
     linkText: 'Learn More',
     href: '/software-vendors',
     imgSrc: vendorsImg,
-    shortText: 'A short text in two rows will be in top.',
-    buttonLabel: 'Learn More',
-    imgWidth: 80,
-  },
-  {
-    title: 'Lif Token',
-    text: 'Lif is used by travel companies to distribute inventory and make transactions without intermediaries.',
-    linkText: 'Learn More',
-    href: '/lif-token',
-    imgSrc: tokenImg,
     shortText: 'A short text in two rows will be in top.',
     buttonLabel: 'Learn More',
     imgWidth: 80,
--- src/commons/LearnMoreCards/index.jsx
+++ src/commons/LearnMoreCards/index.jsx
@@ -22,16 +22,13 @@ type PropsType = {
 
 const LearnMoreCards = ({ main, data = cardsInfo }: PropsType) => (
   <CardDeck>
-    {data.map((info, index) => {
-      if (main && index > 3) return null;
-      return (
-        <Card
-          key={info.href}
-          {...info}
-          main={main}
-        />
-      );
-    })}
+    {data.map(info => (
+      <Card
+        key={info.href}
+        {...info}
+        main={main}
+      />
+    ))}
   </CardDeck>
 );
 export default LearnMoreCards;
