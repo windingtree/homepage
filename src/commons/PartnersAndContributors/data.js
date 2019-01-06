f17f2336748d29f4642fee9cf8cf32b8de499848
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2019-01-06 19:46:47 -0300

    refactor(PartnersAndContributors): Update data, images and names

--- src/commons/PartnersAndContributors/data.js
+++ src/commons/PartnersAndContributors/data.js
@@ -1,50 +1,65 @@
 // @flow
-import rsk from './images/rsk.png';
-import zeppelinOs from './images/zeppelin.svg';
 import nordicChoice from './images/nordic.jpg';
 import airNewZealand from './images/anz.png';
-import erevmax from './images/erevmax.png';
-import siesta from './images/siesta.png';
+import swiss from './images/swiss.svg';
+import luftansaGroup from './images/lufthansa-group.png';
+import rsk from './images/rsk.png';
+import zeppelinOS from './images/zeppelin.svg';
 
-type CollaboratorType = {
-  title: string,
-  text: string,
-  imgSrc: string
+export type CollaboratorType = {
+    name: string,
+    text: string,
+    href: string,
+    imgSrc: string,
+    imgWidth?: number,
+    imgTopPosition?: number,
+    user?: {
+        name: string,
+        role: string,
+        imgSrc: string
+    }
 };
 
-const collaborators: CollaboratorType[] = [
+const quotes: CollaboratorType[] = [
   {
-    title: 'airNewZealand',
+    name: 'nordicChoiceHotels',
+    text: 'Nordic Choice Hotels is currently experimenting with the blockchain. Some of the rooms on Hobo have been made available on the blockchain platform Winding Tree, whose data can be used by third-party apps and sites that turn to the customers.',
+    href: 'https://www.nordicchoicehotels.com/?',
+    imgSrc: nordicChoice,
+    imgWidth: 80,
+  },
+  {
+    name: 'airNewZealand',
     text: 'Making the sales process less complex, Air New Zealand customers can benefit from reduced transactional costs, while the airline can take advantage of the swift and secure sharing of information',
+    href: 'https://www.airnewzealand.eu/',
     imgSrc: airNewZealand,
   },
   {
-    title: 'nordicChoiceHotels',
-    text: 'To completely bypass the booking middlemen such as Expedia and Booking.com, Nordic Choice Hotels is currently experimenting with the blockchain. Some of the rooms on Hobo have been made available on the blockchain platform Windingtree, whose data can be used by third-party apps and sites that turn to the customers.',
-    imgSrc: nordicChoice,
+    name: 'swiss',
+    text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.',
+    href: 'https://www.swiss.com/ready-takeoff-lufthansa-strikes-deal-blockchain-ico-startup/',
+    imgSrc: swiss,
   },
   {
-    title: 'eRevMax',
-    text: 'A software company - supplier of PMS. They are implementing an export of hotel inventory to Winding Tree platform.',
-    imgSrc: erevmax,
+    name: 'luftansaGroup',
+    text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.',
+    href: 'https://lufthansaGroup.com/2017/10/10/lufthansa-partners-with-a-blockchain-provider-in-an-investment-worth-testing/',
+    imgSrc: luftansaGroup,
   },
   {
-    title: 'Siesta Cloud',
-    text: 'Tech company creating PMS. They are our partner and are willing to integrate export to WT as soon as possible. We are also discussing technical details of our data structures and architecture with them.',
-    imgSrc: siesta,
-  },
-  {
-    title: 'RSK',
+    name: 'rsk',
     text: 'Winding Tree will provide a testnet network using the smart contract solutions provided by the RSK network, using a two way payment channels we will be able to have a Lif token and smart contract platform for hotel distribution over bitcoin',
+    href: 'https://www.rsk.co/',
     imgSrc: rsk,
+    imgWidth: 110,
+    imgTopPosition: -11,
   },
   {
-    title: 'Zeppelin OS',
+    name: 'zeppelinOS',
     text: 'Winding Tree will provide its own operative system for upgradeable smart contracts in Ethereum reusing all the solutions provided by ZeppelinOS, having a direct collaboration with them on upgradeable smart contracts, on chain libraries, on chain kernels, decentralized governance, etc.',
-    imgSrc: zeppelinOs,
+    href: 'https://zeppelinos.org/',
+    imgSrc: zeppelinOS,
   },
-
-
 ];
 
-export default collaborators;
+export default quotes;
--- src/commons/PartnersAndContributors/index.jsx
+++ src/commons/PartnersAndContributors/index.jsx
@@ -3,30 +3,41 @@ import React from 'react';
 import {
   Tab, Nav, Col, Image,
 } from '@windingtree/wt-ui-react';
-import collaborators from './data';
+import partners from './data';
 
 type PropsType = {
   tab: number
 };
 
 const PartnersAndContributors = ({ tab }: PropsType) => (
-  <Tab.Container id="collaborators tabs" defaultActiveKey={collaborators[tab].title}>
-    <Nav className="nav-pointers" variant="tabs">
-      {collaborators.map(collaborator => (
-        <Nav.Item as={Col} md={3} key={collaborator.title}>
-          <Nav.Link eventKey={collaborator.title}>
-            <Image fluid src={collaborator.imgSrc} alt={collaborator.title} />
+  <Tab.Container id="partners tabs" defaultActiveKey={partners[tab].name}>
+    <Nav className="nav-pointers col-lg-12" variant="tabs">
+      {partners.map(quote => (
+        <Nav.Item as={Col} className="col-6 col-md-4 col-lg-2">
+          <Nav.Link eventKey={quote.name} className="pb-1 pb-lg-2">
+            <Image
+              fluid
+              src={quote.imgSrc}
+              alt={quote.name}
+              style={{
+                maxWidth: quote.imgWidth,
+                position: 'relative',
+                top: quote.imgTopPosition,
+              }}
+            />
           </Nav.Link>
         </Nav.Item>
       ))}
     </Nav>
-    <Tab.Content className="px-2 py-3">
-      {collaborators.map(collaborator => (
-        <Tab.Pane key={collaborator.title} eventKey={collaborator.title}>
-          <blockquote className="blockquote blockquote-alt mb-0" key={collaborator.title}>
-            <p className="blockquote-body p-0">
-              {collaborator.text}
+    <Tab.Content className="px-2 py-3 bg-white">
+      {partners.map(quote => (
+        <Tab.Pane eventKey={quote.name}>
+          <blockquote className="blockquote blockquote-alt">
+            <p className="blockquote-body">
+              {quote.text}
+              <a href={quote.href}>Read full article</a>
             </p>
+            {quote.user && <QuoteUser {...quote.user} />}
           </blockquote>
         </Tab.Pane>
       ))}
@@ -37,5 +48,25 @@ const PartnersAndContributors = ({ tab }: PropsType) => (
 PartnersAndContributors.defaultProps = {
   tab: 0,
 };
+
+type QuoteUserType = { name: string, role: string, imgSrc: string };
+const QuoteUser = ({ name, role, imgSrc }: QuoteUserType) => (
+  <footer className="blockquote-footer">
+    <div className="user user-sm">
+      <img
+        className="user-img"
+        src={imgSrc}
+        width="60"
+        height="60"
+        alt={name}
+      />
+      <div className="user-data">
+        <p className="user-name">{name}</p>
+        <p className="user-role">{role}</p>
+      </div>
+    </div>
+  </footer>
+);
+
 
 export default PartnersAndContributors;
