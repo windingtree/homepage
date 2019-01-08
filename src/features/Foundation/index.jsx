2915b923322d700d5d3d06ff56c73dc329263b8b
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2019-01-07 22:32:47 -0300

    feat(Foundation): Add Team section

--- src/features/Foundation/index.jsx
+++ src/features/Foundation/index.jsx
@@ -1,12 +1,15 @@
+// @flow
 import React from 'react';
 import Banner from 'commons/Banner';
 import Hero from 'commons/Hero';
+import Team from './Team';
 
 const Foundation = () => (
-  <>
+  <main className="app-content">
     <Hero variant="foundation" />
     <Banner variant="consultancy" />
-  </>
+    <Team />
+  </main>
 );
 
 export default Foundation;
