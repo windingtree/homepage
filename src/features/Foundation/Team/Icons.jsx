a53c2d9cbb7e404c1832a0271f42870c8156e738
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2019-01-07 22:47:08 -0300

    feat(Team): Open links in new tab

--- src/features/Foundation/Team/Icons.jsx
+++ src/features/Foundation/Team/Icons.jsx
@@ -14,44 +14,44 @@ const Icons = ({ icons }: PropsType) => {
     <ul className="d-none d-md-block list-inline">
       {github && (
         <li className="list-inline-item">
-          <a href={github} title="GitHub" className="text-muted text-alpha">
+          <a href={github} title="GitHub" className="text-muted text-alpha" target="_blank" rel="noopener noreferrer">
             <i className="mdi mdi-24px mdi-github-circle" />
           </a>
         </li>)}
       {twitter && (
         <li className="list-inline-item">
-          <a href={twitter} title="Twitter" className="text-muted text-alpha">
+          <a href={twitter} title="Twitter" className="text-muted text-alpha" target="_blank" rel="noopener noreferrer">
             <i className="mdi mdi-24px mdi-twitter" />
           </a>
         </li>)}
       {linkedin && (
         <li className="list-inline-item">
-          <a href={linkedin} title="LinkedIn" className="text-muted text-alpha">
+          <a href={linkedin} title="LinkedIn" className="text-muted text-alpha" target="_blank" rel="noopener noreferrer">
             <i className="mdi mdi-24px mdi-linkedin" />
           </a>
         </li>
       )}
       {link && (
         <li className="list-inline-item">
-          <a href={link} title="Link" className="text-muted text-alpha">
+          <a href={link} title="Link" className="text-muted text-alpha" target="_blank" rel="noopener noreferrer">
             <i className="mdi mdi-24px mdi-link-variant" />
           </a>
         </li>)}
       {facebook && (
         <li className="list-inline-item">
-          <a href={facebook} title="Facebook" className="text-muted text-alpha">
+          <a href={facebook} title="Facebook" className="text-muted text-alpha" target="_blank" rel="noopener noreferrer">
             <i className="mdi mdi-24px mdi-facebook" />
           </a>
         </li>)}
       {vk && (
         <li className="list-inline-item">
-          <a href={vk} title="VK" className="text-muted text-alpha">
+          <a href={vk} title="VK" className="text-muted text-alpha" target="_blank" rel="noopener noreferrer">
             <i className="mdi mdi-24px mdi-vk" />
           </a>
         </li>)}
       {bitBucket && (
         <li className="list-inline-item">
-          <a href={bitBucket} title="Bitbucket" className="text-muted text-alpha">
+          <a href={bitBucket} title="Bitbucket" className="text-muted text-alpha" target="_blank" rel="noopener noreferrer">
             <i className="mdi mdi-24px mdi-bitbucket" />
           </a>
         </li>)}
