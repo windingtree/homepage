8bef4f844484ca0bd8fd074460663b1a3791565f
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2019-01-09 12:51:41 -0300

    feat(Foundation): Add Roadmap

--- src/features/Foundation/index.jsx
+++ src/features/Foundation/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from 'commons/Banner';
 import Hero from 'commons/Hero';
+import Roadmap from './Roadmap';
 
 const Foundation = () => (
   <>
     <Hero variant="foundation" />
     <Banner variant="consultancy" />
+    <Roadmap />
   </>
 );
 
