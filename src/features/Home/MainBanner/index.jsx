c9dc948f7deb16a804568a72292b9d8e23ad1eb2
Alexander <menschikov@gmail.com>
2019-01-14 13:26:27 +0500

    fixed Hero banners to hide image on sm, centered buttons on sm

--- src/features/Home/MainBanner/index.jsx
+++ src/features/Home/MainBanner/index.jsx
@@ -15,8 +15,6 @@ const MainBanner = () => (
             <Col md={9}>
               <h1 className="mb-1 text-white">
               Winding Tree
-                <br />
-              Decentralized Travel Ecosystem
               </h1>
             </Col>
             <Col md={6}>
@@ -28,7 +26,7 @@ const MainBanner = () => (
               <ul className="d-flex flex-column flex-md-row justify-content-md-center align-items-md-center list-inline">
                 <li className="mb-1 mb-lg-0 ml-0 mr-md-1">
                   <Button href="https://docs.google.com/forms/d/e/1FAIpQLSevHx6goFLdbdI7TSX6JFugNlT56nlRD7I1XjqGc-06pIoemA/viewform" block>
-                  Get on board
+                  Join Ecosystem
                   </Button>
                 </li>
                 <li className="mb-1 mb-lg-0 list-inline-item">
