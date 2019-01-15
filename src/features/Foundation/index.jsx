68c748ff7cca7fda9fff65e83c349edc50f9491e
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2019-01-15 02:43:37 -0300

    feat(Foundation): Add About section

--- src/features/Foundation/index.jsx
+++ src/features/Foundation/index.jsx
@@ -1,16 +1,20 @@
 // @flow
 import React from 'react';
 import Banner from 'commons/Banner';
+import Events from 'commons/Events';
 import Hero from 'commons/Hero';
 import Team from './Team';
+import About from './About';
 import { members, advisors } from './data';
 
 const Foundation = () => (
   <main className="app-content">
     <Hero variant="foundation" />
-    <Banner variant="consultancy" />
+    <About id="about" />
     <Team title="Team" members={members} />
     <Team title="Advisors" members={advisors} />
+    <Banner variant="consultancy" />
+    <Events />
   </main>
 );
 
