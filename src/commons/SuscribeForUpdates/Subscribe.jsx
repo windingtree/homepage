7e47714eec5ecb211b349563293d1ea56a0cf2dd
skkr <nico.sikker@gmail.com>
2019-01-23 22:14:06 -0300

    Made the whole subscription form clickable and added title tags

--- src/commons/SuscribeForUpdates/Subscribe.jsx
+++ src/commons/SuscribeForUpdates/Subscribe.jsx
@@ -7,24 +7,33 @@ const Subscribe = () => (
     <h3 className="mb-1 mb-md-2">
         Subscribe for updates
     </h3>
-    <InputGroup className="mb-3">
-      <Form.Control
-        placeholder="Enter e-mail"
-        aria-label="Enter e-mail"
-      />
-      <InputGroup.Append>
-        <Button
-          href="https://windingtree.us14.list-manage.com/subscribe/post?u=93dec359c99b9871f7330768c&id=2f41b0318a"
-          target="_blank"
-          variant="accent"
-          type="submit"
-          size="lg"
-          block
-        >
-          Subscribe
-        </Button>
-      </InputGroup.Append>
-    </InputGroup>
+    <div className="cover-btn">
+      <Button
+        href="https://windingtree.us14.list-manage.com/subscribe/post?u=93dec359c99b9871f7330768c&id=2f41b0318a"
+        target="_blank"
+        type="buton"
+        title="Click to subscribe"
+      >Subscribe Form</Button>
+      <InputGroup className="mb-2 mb-md-0">
+        <Form.Control
+          placeholder="Enter e-mail"
+          aria-label="Enter e-mail"
+        />
+        <InputGroup.Append>
+          <Button
+            href="https://windingtree.us14.list-manage.com/subscribe/post?u=93dec359c99b9871f7330768c&id=2f41b0318a"
+            target="_blank"
+            variant="accent"
+            type="submit"
+            size="lg"
+            block
+            title="Click to subscribe"
+          >
+            Subscribe
+          </Button>
+        </InputGroup.Append>
+      </InputGroup>
+    </div>
   </section>
 );
 
