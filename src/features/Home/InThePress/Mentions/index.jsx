2c088e815d81d9a24579e0f1857e98aca899d27d
skkr <nico.sikker@gmail.com>
2019-01-23 22:23:22 -0300

    (improvement) gallery logos are now aligned to the vertical in sm

--- src/features/Home/InThePress/Mentions/index.jsx
+++ src/features/Home/InThePress/Mentions/index.jsx
@@ -8,7 +8,7 @@ const Mentions = () => (
   <ul className="logo-gallery mb-0 justify-content-center">
     {mentions.map((mention, idx) => (
       // eslint-disable-next-line react/no-array-index-key
-      <li key={idx} className="logo-gallery-item">
+      <li key={idx} className="logo-gallery-item align-items-center d-flex">
         <a href={mention.href} target="_blank" rel="noopener noreferrer">
           <img src={mention.imgSrc} alt="" />
         </a>
