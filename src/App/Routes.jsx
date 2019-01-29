48c0825f1ef7ecc1feeb59e24ccc9a6c26f8f961
Nico Calabrese <nico.sikker@gmail.com>
2019-01-29 19:04:10 -0300

    Set routes and template for 404 error page

--- src/App/Routes.jsx
+++ src/App/Routes.jsx
@@ -8,19 +8,21 @@ import SoftwareVendors from 'features/SoftwareVendors';
 import Main from 'features/Main';
 import LifToken from 'features/LifToken';
 import Community from 'features/StartupsAndDevelopers';
+import Page404 from 'features/Page404';
 import ScrollHandler from './ScrollHandler';
 
 const Routes = () => (
   <ScrollHandler>
     <Main>
       <Switch>
+        <Route path="/" exact component={Home} />
         <Route path={['/foundation', '/foundation.html']} component={Foundation} />
         <Route path={['/suppliers', '/suppliers.html']} component={Suppliers} />
         <Route path={['/sellers', '/sellers.html']} component={Sellers} />
         <Route path={['/software-vendors', '/software-vendors.html']} component={SoftwareVendors} />
         <Route path={['/lif-token', '/lif-token.html']} component={LifToken} />
         <Route path={['/startups-and-developers', '/startups-and-developers.html']} component={Community} />
-        <Route component={Home} />
+        <Route path="*" component={Page404} />
       </Switch>
     </Main>
   </ScrollHandler>
