fb159f26102dbb2bf87c33536bd9e88d1841f973
Alexander <menschikov@gmail.com>
2019-08-08 08:39:56 +0500

    news updated

--- data/commons/NewsSection/index.js
+++ data/commons/NewsSection/index.js
@@ -3,16 +3,16 @@ const NewsSection =  {
     header : "Latest From Blog",
     items:[
         {
-            articleUrl: "https://blog.windingtree.com/were-pioneering-travel-tech-business-hackathon-tickets-are-open-ba6489078bca",
-            articleTitle:"We’re pioneering Travel Tech Business Hackathon — tickets are open!",
-            articleDescription:"Join business teams from airlines, hotels, travel startups & software vendors to work on Proof-of-Concept proposals — no coding or technical background required!",
-            thumbnail: require('./img/hackathon.png'),
+            articleUrl: "https://blog.windingtree.com/weekly-development-update-7-22-to-7-29-f869cd5bfb93",
+            articleTitle:"Weekly Development Update 7/22 to 7/29",
+            articleDescription:"",
+            thumbnail: require('./img/repo.png'),
         },
         {
-            articleUrl: "https://blog.windingtree.com/announcing-official-hacktravel-lisbon-2019-hackathon-challenges-9cc8a352e121",
-            articleTitle:"Announcing Official #HackTravel Lisbon 2019 Hackathon Challenges",
-            articleDescription:"review challenges",
-            thumbnail: require('./img/hackathon.png'),
+            articleUrl: "https://blog.windingtree.com/weekly-development-update-7-14-7-22-b180ffbeee0f",
+            articleTitle:"Weekly Development Update 7/14 to 7/22",
+            articleDescription:"",
+            thumbnail: require('./img/repo.png'),
         },
         {
             articleUrl: "https://blog.windingtree.com/building-trust-on-a-trustless-blockchain-ba71872f8541",
